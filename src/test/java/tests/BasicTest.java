1ec70e4ed6c850cdd0934ea0ae6cd7c6a5e1f117
Ana Cvetkovic <acvetkovic87@gmail.com>
2023-10-24 21:26:14 +0200

    Add Basic Test - project configuration

---
+++ src/test/java/tests/BasicTest.java
@@ -0,0 +1,55 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import pages.NavPage;
+
+import java.time.Duration;
+
+public abstract class BasicTest {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected String baseUrl = "https://vue-demo.daniel-avellaneda.com";
+    protected NavPage navPage;
+
+
+    @BeforeClass
+    public void setup(){
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.manage().window().maximize();
+
+        navPage = new NavPage(driver, wait);
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        driver.navigate().to(baseUrl);
+    }
+
+    @AfterMethod
+    public void afterMethod() throws InterruptedException{
+        Thread.sleep(2000);
+        driver.manage().deleteAllCookies();
+        ((JavascriptExecutor) driver).executeScript("window.localStorage.clear();");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        driver.quit();
+    }
+}
