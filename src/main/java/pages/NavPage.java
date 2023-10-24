5a40efcb945af3c04ba274307eea561d66d858ac
Ana Cvetkovic <acvetkovic87@gmail.com>
2023-10-24 21:37:06 +0200

    Add NavPage - interacting with web elements: navbar

---
+++ src/main/java/pages/NavPage.java
@@ -0,0 +1,40 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class NavPage extends BasicPage{
+    public NavPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    public WebElement getLanguagesButton(){
+        return driver.findElement(By.className("btnLocaleActivation"));
+    }
+    public void clickOnLanguagesButton(){
+        getLanguagesButton().click();
+    }
+
+    public WebElement getEnButton(){
+        return driver.findElement(By.className("btnEN"));
+    }
+    public void clickOnEnButton(){
+        getEnButton().click();
+    }
+
+    public void waitForLanguagesMenuToBeVisible(){
+        wait
+                .withMessage("Languages menu should be visible.")
+                .until(ExpectedConditions.visibilityOfElementLocated(By.className("v-menu__content")));
+    }
+
+    public WebElement getLoginButton(){
+        return driver.findElement(By.cssSelector("header a[href='/login']"));
+    }
+    public void clickOnLoginButton(){
+        getLoginButton().click();
+    }
+}
