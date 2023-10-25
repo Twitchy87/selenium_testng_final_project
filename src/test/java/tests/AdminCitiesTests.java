f79b03b02f6089495d89b8908a037a0ae12de3a2
Ana Cvetkovic <acvetkovic87@gmail.com>
2023-10-25 18:52:41 +0200

    Add AdminCitiesTests - edit city

--- src/test/java/tests/AdminCitiesTests.java
+++ src/test/java/tests/AdminCitiesTests.java
@@ -5,6 +5,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import retry.RetryAnalyzer;
 
+import javax.swing.plaf.TableHeaderUI;
+
 public class AdminCitiesTests extends BasicTest{
 
     @Test(priority = 1, retryAnalyzer = RetryAnalyzer.class)
@@ -86,6 +88,58 @@ public class AdminCitiesTests extends BasicTest{
         citiesPage.enterNewCity(city);
         citiesPage.waitForSaveButtonToBeClickable();
         citiesPage.clickOnSaveButton();
+        messagePopUpPage.waitForSuccessfulPopUpToBeVisible();
+        Assert.assertTrue(messagePopUpPage.getSuccessfulPopUpMessage().contains("Saved successfully"),
+                "Successful pop up message should be \"Saved successfully\"");
+
+    }
+
+    @Test(priority = 4, retryAnalyzer = RetryAnalyzer.class)
+    public void editCity() throws InterruptedException {
+        String email = "admin@admin.com";
+        String password = "12345";
+        String oldCity = "Madrid";
+        String newCity = "Alicante";
+
+        navPage.clickOnLoginButton();
+
+        wait
+                .withMessage("Url should be " + baseUrl + "/login")
+                .until(ExpectedConditions.urlContains("/login"));
+
+        loginPage.login(email, password);
+
+        navPage.clickOnAdminButton();
+
+        navPage.waitForAdminMenuToBeVisible();
+        navPage.clickOnCitiesButton();
+
+        wait
+                .withMessage("Url should be " + baseUrl + "/admin/cities")
+                .until(ExpectedConditions.urlContains("/admin/cities"));
+
+        citiesPage.clickOnNewItemButton();
+        citiesPage.waitForNewItemDialogToBeVisible();
+        citiesPage.enterNewCity(oldCity);
+        citiesPage.waitForSaveButtonToBeClickable();
+        citiesPage.clickOnSaveButton();
+
+        Thread.sleep(2000);
+
+        citiesPage.searchForCity(oldCity);
+        citiesPage.waitForSearchForCityResultToBeOne();
+
+        Thread.sleep(2000);
+
+        citiesPage.clickOnEditButton();
+        citiesPage.enterEditedItem(newCity);
+
+        Thread.sleep(2000);
+
+        citiesPage.clickOnSaveButton();
+
+        Thread.sleep(2000);
+
         messagePopUpPage.waitForSuccessfulPopUpToBeVisible();
         Assert.assertTrue(messagePopUpPage.getSuccessfulPopUpMessage().contains("Saved successfully"),
                 "Successful pop up message should be \"Saved successfully\"");
