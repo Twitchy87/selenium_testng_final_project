24c6ba0462b90d3c0d86f2ed53b7bd64ce472692
Ana Cvetkovic <acvetkovic87@gmail.com>
2023-10-25 16:02:19 +0200

    Add AdminCitiesTests - checks input types for create or edit new city

--- src/test/java/tests/AdminCitiesTests.java
+++ src/test/java/tests/AdminCitiesTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import retry.RetryAnalyzer;
 
@@ -27,5 +28,33 @@ public class AdminCitiesTests extends BasicTest{
         wait
                 .withMessage("Url should be " + baseUrl + "/admin/cities")
                 .until(ExpectedConditions.urlContains("/admin/cities"));
+    }
+
+    @Test(priority = 2, retryAnalyzer = RetryAnalyzer.class)
+    public void checksInputTypesForCreateOrEditNewCity(){
+        String email = "admin@admin.com";
+        String password = "12345";
+
+        navPage.clickOnLoginButton();
+
+        wait
+                .withMessage("Url should be " + baseUrl + "/login")
+                .until(ExpectedConditions.urlContains("/login"));
+
+        loginPage.login(email, password);
+
+        navPage.clickOnAdminButton();
+
+        navPage.waitForAdminMenuToBeVisible();
+        navPage.clickOnCitiesButton();
+
+        wait
+                .withMessage("Url should be " + baseUrl + "/admin/cities")
+                .until(ExpectedConditions.urlContains("/admin/cities"));
+
+        citiesPage.clickOnNewItemButton();
+        citiesPage.waitForNewItemDialogToBeVisible();
+        Assert.assertEquals(citiesPage.getNewItemNameInputType(), "text",
+                "New Item name input type should be text.");
     }
 }
