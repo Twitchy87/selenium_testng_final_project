d6610583940cc7cda1947e90d1539a7d70ad840c
Ana Cvetkovic <acvetkovic87@gmail.com>
2023-10-25 18:50:05 +0200

    Updated CitiesPage - added search and edit city

--- src/main/java/pages/CitiesPage.java
+++ src/main/java/pages/CitiesPage.java
@@ -1,10 +1,14 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CitiesPage extends BasicPage{
     public CitiesPage(WebDriver driver, WebDriverWait wait) {
@@ -46,6 +50,44 @@ public class CitiesPage extends BasicPage{
         wait
                 .withMessage("Save button should be clickable.")
                 .until(ExpectedConditions.elementToBeClickable(getSaveButton()));
+    }
+
+    public WebElement getSearchInput(){
+        return driver.findElement(By.id("search"));
+    }
+    public void searchForCity(String city){
+        getSearchInput().clear();
+        getSearchInput().sendKeys(city);
+    }
+
+    public void waitForSearchForCityResultToBeOne(){
+        wait.withMessage("One city should be displayed after search.")
+                .until(ExpectedConditions.numberOfElementsToBe(By.cssSelector("tbody tr"), 1));
+    }
+
+    public WebElement getEditButton(){
+        return driver.findElement(By.id("edit"));
+    }
+    public void clickOnEditButton(){
+        getEditButton().click();
+    }
+
+    public WebElement getEditItemInput(){
+        return driver.findElement(By.id("name"));
+    }
+    public void enterEditedItem(String newCity) throws InterruptedException {
+        List<Character> chars = new ArrayList<>();
+        for (char ch : newCity.toCharArray()) {
+
+            chars.add(ch);
+        }
+
+        for (char ch : chars){
+            getEditItemInput().sendKeys(Keys.BACK_SPACE);
+        }
+        Thread.sleep(2000);
+        getEditItemInput().sendKeys(newCity);
+
     }
 
 }
