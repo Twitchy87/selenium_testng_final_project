ddcba99e6541f29d7cda56d103ebcb39898e7e1d
Ana Cvetkovic <acvetkovic87@gmail.com>
2023-10-25 20:21:55 +0200

    Add AdminCitiesTests - delete city

--- src/test/java/tests/AdminCitiesTests.java
+++ src/test/java/tests/AdminCitiesTests.java
@@ -5,7 +5,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import retry.RetryAnalyzer;
 
-import javax.swing.plaf.TableHeaderUI;
 
 public class AdminCitiesTests extends BasicTest{
 
@@ -197,5 +196,72 @@ public class AdminCitiesTests extends BasicTest{
         citiesPage.waitForSearchForCityResultToBeOne();
         Assert.assertEquals(citiesPage.getNameOfCity(), newCity,
                 "Result city should be " + newCity);
+    }
+
+    @Test(priority = 6, retryAnalyzer = RetryAnalyzer.class)
+    public void deleteCity() throws InterruptedException {
+        String email = "admin@admin.com";
+        String password = "12345";
+        String oldCity = "Madrid";
+        String newCity = "Alicante";
+
+        navPage.clickOnLoginButton();
+
+        wait
+                .withMessage("Url should be " + baseUrl + "/login")
+                .until(ExpectedConditions.urlContains("/login"));
+
+        loginPage.login(email, password);
+
+        navPage.clickOnAdminButton();
+
+        navPage.waitForAdminMenuToBeVisible();
+        navPage.clickOnCitiesButton();
+
+        wait
+                .withMessage("Url should be " + baseUrl + "/admin/cities")
+                .until(ExpectedConditions.urlContains("/admin/cities"));
+
+        citiesPage.clickOnNewItemButton();
+        citiesPage.waitForNewItemDialogToBeVisible();
+        citiesPage.enterNewCity(oldCity);
+        citiesPage.waitForSaveButtonToBeClickable();
+        citiesPage.clickOnSaveButton();
+
+        Thread.sleep(2000);
+
+        citiesPage.searchForCity(oldCity);
+        citiesPage.waitForSearchForCityResultToBeOne();
+
+        Thread.sleep(2000);
+
+        citiesPage.clickOnEditButton();
+        citiesPage.enterEditedItem(newCity);
+
+        Thread.sleep(2000);
+
+        citiesPage.clickOnSaveButton();
+        citiesPage.clickOnClearSearchButton();
+
+        Thread.sleep(2000);
+
+        citiesPage.searchForCity(newCity);
+        citiesPage.waitForSearchForCityResultToBeOne();
+        Assert.assertEquals(citiesPage.getNameOfCity(), newCity,
+                "Result city should be " + newCity);
+
+        citiesPage.clickOnDeleteButton();
+        messagePopUpPage.waitForDeleteWarningDialogToBeVisible();
+
+        Thread.sleep(2000);
+
+        messagePopUpPage.clickOnDeleteButton();
+
+        Thread.sleep(2000);
+
+        messagePopUpPage.waitForSuccessfulPopUpToBeVisible();
+        Assert.assertTrue(messagePopUpPage.getSuccessfulPopUpMessage().contains("Deleted successfully"),
+                "Successful pop up message should be \"Deleted successfully\"");
+
     }
 }
