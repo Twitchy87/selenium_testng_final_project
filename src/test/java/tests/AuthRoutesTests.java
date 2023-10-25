7449a7dc0214d8bdb02b5bbc34616ca248e03d53
Ana Cvetkovic <acvetkovic87@gmail.com>
2023-10-25 23:17:52 +0200

    Add AuthRoutesTests - forbids visits to profile url if not authenticated

--- src/test/java/tests/AuthRoutesTests.java
+++ src/test/java/tests/AuthRoutesTests.java
@@ -14,4 +14,13 @@ public class AuthRoutesTests extends BasicTest{
                 .withMessage("Url should be " + baseUrl + "/login")
                 .until(ExpectedConditions.urlToBe(baseUrl + "/login"));
     }
+
+    @Test(priority = 2, retryAnalyzer = RetryAnalyzer.class)
+    public void forbidsVisitsToProfileUrlIfNotAuthenticated(){
+        driver.navigate().to(baseUrl + "/profile");
+
+        wait
+                .withMessage("Url should be " + baseUrl + "/login")
+                .until(ExpectedConditions.urlToBe(baseUrl + "/login"));
+    }
 }
