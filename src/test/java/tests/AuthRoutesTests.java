7ef35793a8799c5f7a3d92e4b212a143b70959a4
Ana Cvetkovic <acvetkovic87@gmail.com>
2023-10-25 23:10:44 +0200

    Add AuthRoutesTests - forbids visits to home url if not authenticated

---
+++ src/test/java/tests/AuthRoutesTests.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+import retry.RetryAnalyzer;
+
+public class AuthRoutesTests extends BasicTest{
+
+    @Test(priority = 1, retryAnalyzer = RetryAnalyzer.class)
+    public void forbidsVisitsToHomeUrlIfNotAuthenticated(){
+        driver.navigate().to(baseUrl + "/home");
+
+        wait
+                .withMessage("Url should be " + baseUrl + "/login")
+                .until(ExpectedConditions.urlToBe(baseUrl + "/login"));
+    }
+}
