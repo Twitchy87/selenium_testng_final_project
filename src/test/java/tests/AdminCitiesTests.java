7eb4c3409f52e57d3e9bb0f240b1aa7b5be06fc1
Ana Cvetkovic <acvetkovic87@gmail.com>
2023-10-25 19:39:12 +0200

    Add AdminCitiesTests - search city

--- src/test/java/tests/AdminCitiesTests.java
+++ src/test/java/tests/AdminCitiesTests.java
@@ -145,4 +145,57 @@ public class AdminCitiesTests extends BasicTest{
                 "Successful pop up message should be \"Saved successfully\"");
 
     }
+
+    @Test(priority = 5, retryAnalyzer = RetryAnalyzer.class)
+    public void searchCity() throws InterruptedException {
+        String email = "admin@admin.com";
+        String password = "12345";
+        String oldCity = "Madrid";
+        String newCity = "Alicante";
+
+        navPage.clickOnLoginButton();
+
+        wait
+                .withMessage("Url should be " + baseUrl + "/login")
+                .until(ExpectedConditions.urlContains("/login"));
+
+        loginPage.login(email, password);
+
+        navPage.clickOnAdminButton();
+
+        navPage.waitForAdminMenuToBeVisible();
+        navPage.clickOnCitiesButton();
+
+        wait
+                .withMessage("Url should be " + baseUrl + "/admin/cities")
+                .until(ExpectedConditions.urlContains("/admin/cities"));
+
+        citiesPage.clickOnNewItemButton();
+        citiesPage.waitForNewItemDialogToBeVisible();
+        citiesPage.enterNewCity(oldCity);
+        citiesPage.waitForSaveButtonToBeClickable();
+        citiesPage.clickOnSaveButton();
+
+        Thread.sleep(2000);
+
+        citiesPage.searchForCity(oldCity);
+        citiesPage.waitForSearchForCityResultToBeOne();
+
+        Thread.sleep(2000);
+
+        citiesPage.clickOnEditButton();
+        citiesPage.enterEditedItem(newCity);
+
+        Thread.sleep(2000);
+
+        citiesPage.clickOnSaveButton();
+        citiesPage.clickOnClearSearchButton();
+
+        Thread.sleep(2000);
+
+        citiesPage.searchForCity(newCity);
+        citiesPage.waitForSearchForCityResultToBeOne();
+        Assert.assertEquals(citiesPage.getNameOfCity(), newCity,
+                "Result city should be " + newCity);
+    }
 }
