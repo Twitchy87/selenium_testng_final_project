07785b53cacc3c30f7d49959db5e58e8f054680c
Ana Cvetkovic <acvetkovic87@gmail.com>
2023-10-26 17:05:04 +0200

    Add ProfileTests - visits the profile page

---
+++ src/test/java/tests/ProfileTests.java
@@ -0,0 +1,43 @@
+package tests;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+import retry.RetryAnalyzer;
+
+public class ProfileTests extends BasicTest{
+
+    @Test(priority = 1, retryAnalyzer = RetryAnalyzer.class)
+    public void visitsTheProfilePage(){
+        String email = "admin@admin.com";
+        String password = "12345";
+
+        navPage.clickOnLoginButton();
+
+        wait
+                .withMessage("Url should be " + baseUrl + "/login")
+                .until(ExpectedConditions.urlContains("/login"));
+
+        loginPage.login(email, password);
+
+        wait
+                .withMessage("Url should be " + baseUrl + "/home")
+                .until(ExpectedConditions.urlContains("/home"));
+
+        navPage.clickOnMyProfileLink();
+
+        wait
+                .withMessage("Url should be " + baseUrl + "/profile")
+                .until(ExpectedConditions.urlContains("/profile"));
+
+
+//        Assert.assertEquals(profilePage.getEmailInputValue(),
+//                "admin@admin.com",
+//                "Email input value should be \"admin@admin.com\"");
+
+        navPage.clickOnLogoutButton();
+
+        wait
+                .withMessage("Url should be " + baseUrl + "/login")
+                .until(ExpectedConditions.urlContains("/login"));
+    }
+}
