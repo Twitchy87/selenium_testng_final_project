d516f2bcad10fe8c17ae3793f984caab46ccbe3f
Ana Cvetkovic <acvetkovic87@gmail.com>
2023-10-27 13:46:23 +0200

    Updated ProfilePage - added form input elements

--- src/main/java/pages/ProfilePage.java
+++ src/main/java/pages/ProfilePage.java
@@ -20,4 +20,54 @@ public class ProfilePage extends BasicPage{
                 .until(ExpectedConditions.textToBePresentInElementValue(
                         getEmailInput(), email));
     }
+    public String getEmailInputType(){
+        return getEmailInput().getAttribute("type");
+    }
+    public boolean disabledEmailInput(){
+        return getEmailInput().isEnabled();
+    }
+
+    public WebElement getNameInput(){
+        return driver.findElement(By.id("name"));
+    }
+    public String getNameInputType(){
+        return getNameInput().getAttribute("type");
+    }
+
+    public WebElement getPhoneInput(){
+        return driver.findElement(By.id("phone"));
+    }
+    public String getPhoneInputType(){
+        return getPhoneInput().getAttribute("type");
+    }
+
+    public WebElement getCityInput(){
+        return driver.findElement(By.id("city"));
+    }
+    public String getCityInputType(){
+        return getCityInput().getAttribute("type");
+    }
+
+    public WebElement getCountryInput(){
+        return driver.findElement(By.id("country"));
+    }
+    public String getCountryInputType(){
+        return getCountryInput().getAttribute("type");
+    }
+
+    public WebElement getUrlTwitterInput(){
+        return driver.findElement(By.id("urlTwitter"));
+    }
+    public String getUrlTwitterInputType(){
+        return getUrlTwitterInput().getAttribute("type");
+    }
+
+    public WebElement getUrlGitHubInput(){
+        return driver.findElement(By.id("urlGitHub"));
+    }
+    public String getUrlGitHubInputType(){
+        return getUrlGitHubInput().getAttribute("type");
+    }
+
+
 }
